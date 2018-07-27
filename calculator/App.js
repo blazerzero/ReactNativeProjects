c09fdf469c9cde99dabd5c917fb5a3e30ca02ac8
blazerzero <habibelo@oregonstate.edu>
2018-07-27 00:39:20 -0700

    [calculator] halfway functionality for top 4 rows of buttons

--- calculator/App.js
+++ calculator/App.js
@@ -5,34 +5,126 @@ import {
   Platform,
   Text,
   View,
-  Clipboard
-} from 'react-native';
+  Clipboard,
+} from 'react-native'
 import { ButtonGroup } from 'react-native-elements';
 import Toast, {DURATION} from 'react-native-easy-toast';
 import { Constants } from 'expo';
 
-global.result = 0;
-global.operand2 = 0;
+global.result = '0';
+global.operand2 = '0';
+global.operator = "";
+global.op1made = false;
 global.calculated = true;
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: '0'
+    }
+  }
+  
+  updateText = (newValue) => {
+    this.setState({result: newValue.toString()})
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Toast ref="toast"/>
-        <ButtonGroup 
-          buttons={[global.result]} 
-          containerStyle={styles.result} 
-          textStyle={{color: 'white', fontSize: 72}}
+        <Toast ref="toast"/> 
+        <Text
+          style={styles.result}
           onPress={() => {
             Clipboard.setString(global.result.toString());
             this.refs.toast.show('Copied to clipboard!', DURATION.LENGTH_LONG);
+          }}>
+          {this.state.result}
+        </Text>
+        <ButtonGroup 
+          buttons={['+', '-', 'x', '÷', 'c']} 
+          containerStyle={styles.padrow} 
+          textStyle={{fontSize: 64}}
+          onPress={(index) => {
+            this.setState({index});
+            if (index == 0) {
+              if (global.op1made) global.result = parseInt(global.result) + parseInt(global.operand2);
+              this.updateText(global.result);
+            } 
+            else if (index == 1) {
+              if (global.op1made) global.result = parseInt(global.result) - parseInt(global.operand2);
+              this.updateText(global.result);
+            }
+            else if (index == 2) {
+              if (global.op1made) global.result = parseInt(global.result) * parseInt(global.operand2);
+              this.updateText(global.result);
+            }
+            else if (index == 3) {
+              if (global.op1made) global.result = parseInt(global.result) / parseInt(global.operand2);
+              this.updateText(global.result);
+            }
+            else if (index == 4) {
+              if (global.op1made) global.operand2 = 0;
+              else global.result = 0;
+              this.updateText(0);
+            }
+            global.op1made = true;
           }}/>
-        <ButtonGroup buttons={['+', '-', 'x', '÷', 'c']} containerStyle={styles.padrow} textStyle={{fontSize: 64}}/>
-        <ButtonGroup buttons={['7', '8', '9']} containerStyle={styles.padrow} textStyle={{fontSize: 64}}/>
-        <ButtonGroup buttons={['4', '5', '6']} containerStyle={styles.padrow} textStyle={{fontSize: 64}}/>
-        <ButtonGroup buttons={['1', '2', '3']} containerStyle={styles.padrow} textStyle={{fontSize: 64}}/>
-        <ButtonGroup buttons={['+/-', '0', '=']} containerStyle={styles.padrow} textStyle={{fontSize: 64}}/>
+        <ButtonGroup 
+          buttons={['7', '8', '9']}
+          containerStyle={styles.padrow}
+          textStyle={{fontSize: 64}}
+          onPress={(index) => {
+            var values = [7, 8, 9];
+            if (!global.op1made) {
+              global.result += values[index];
+              if (global.result[0] == 0) global.result = global.result.substring(1);
+              this.updateText(global.result);
+            }
+            else {
+              global.operand2 += values[index];
+              if (global.operand2[0] == 0) global.operand2 = global.operand2.substring(1);
+              this.updateText(global.operand2);
+            }
+          }}/>
+        <ButtonGroup 
+          buttons={['4', '5', '6']} 
+          containerStyle={styles.padrow} 
+          textStyle={{fontSize: 64}}
+          onPress={(index) => {
+            var values = [4, 5, 6];
+            if (!global.op1made) {
+              global.result += values[index];
+              if (global.result[0] == 0) global.result = global.result.substring(1);
+              this.updateText(global.result);
+            }
+            else {
+              global.operand2 += values[index];
+              if (global.operand2[0] == 0) global.operand2 = global.operand2.substring(1);
+              this.updateText(global.operand2);
+            }
+          }}/>
+        <ButtonGroup 
+          buttons={['1', '2', '3']} 
+          containerStyle={styles.padrow} 
+          textStyle={{fontSize: 64}}
+          onPress={(index) => {
+            var values = [1, 2, 3];
+            if (!global.op1made) {
+              global.result += values[index];
+              if (global.result[0] == 0) global.result = global.result.substring(1);
+              this.updateText(global.result);
+            }
+            else {
+              global.operand2 += values[index];
+              if (global.operand2[0] == 0) global.operand2 = global.operand2.substring(1);
+              this.updateText(global.operand2);
+            }
+          }}/>
+        <ButtonGroup 
+          buttons={['+/-', '0', '=']} 
+          containerStyle={styles.padrow} 
+          textStyle={{fontSize: 64}}/>
       </View>
     );
   }
@@ -50,10 +142,15 @@ const styles = StyleSheet.create({
   result: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'stretch',
+    textAlign: 'right',
     backgroundColor: 'black',
+    color: 'white',
+    fontSize: 72,
+    fontWeight: 'bold',
     flexDirection: 'row',
     padding: 10,
+    margin: 10
   },
   padrow: {
     flex: 1,
